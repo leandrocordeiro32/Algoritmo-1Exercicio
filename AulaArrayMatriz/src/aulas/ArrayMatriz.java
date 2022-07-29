37ab6e422e4f8efb7ab72ba83935fd583fc3ac60
leandrocordeiro32 <leandro.cordeiro32@gmail.com>
2022-07-29 19:00:53 -0300

    Atividade Array Multi

--- AulaArrayMatriz/src/aulas/ArrayMatriz.java
+++ AulaArrayMatriz/src/aulas/ArrayMatriz.java
@@ -51,21 +51,32 @@ public class ArrayMatriz {
 	
 	//Array Multidimensional
 	
-	int [][] numerosArray = new int [3][3];
+	int[][] numerosArray = new int[3][3];
+	int[][] num = {{1 , 2 , 3} , {3 , 2 , 1} , {2 , 1 , 3}};
 	int contador = 10;
 	
-	for (int i = 0 ; i < numerosArray.length ; i++) {
-		
-		for(int j = 0 ; j < numerosArray.length ; j++) {
-			
-			numerosArray[i][j] = contador;
-			System.out.print(numerosArray[i][j] + "\t");
-			contador++;
+	for (int i = 0 ; i < num.length ; i++) {
+		for(int j = 0 ; j < num.length ; j++) {
+			System.out.print(num[i][j]+ "\t");
 		}
 		
 		System.out.println("\n");
 	}
-		
+	
+
+	for (int i = 0; i < numerosArray.length; i++) {
+
+		for (int j = 0; j < numerosArray.length; j++) {
+
+			numerosArray[i][j] = contador;
+			System.out.print(numerosArray[i][j] + "\t");
+			contador++;
+		}
+
+		System.out.println("\n");
+	}
+	
+	System.out.print(numerosArray[0][0] + "\n \t" + numerosArray[1][1] + "\n \t \t" + numerosArray[2][2]);
 
 	sc.close();
 	
--- AulaArrayMatriz/src/aulas/ArrayMultidimensional.java
+++ AulaArrayMatriz/src/aulas/ArrayMultidimensional.java
@@ -5,19 +5,8 @@ public class ArrayMultidimensional {
 	public static void main(String[] args) {
 
 		int[][] numerosArray = new int[3][3];
-		int[][] num = {{1 , 2 , 3} , {3 , 2 , 1} , {2 , 1 , 3}};
 		int contador = 10;
 		
-		/*for (int i = 0 ; i < num.length ; i++) {
-			for(int j = 0 ; j < num.length ; j++) {
-				System.out.print(num[i][j]+ "\t");
-			}
-			
-			System.out.println("\n");
-
-		}*/
-		
-
 		for (int i = 0; i < numerosArray.length; i++) {
 
 			for (int j = 0; j < numerosArray.length; j++) {
@@ -30,7 +19,7 @@ public class ArrayMultidimensional {
 			System.out.println("\n");
 		}
 		
-		System.out.print(numerosArray[0][0] + "\n \t" + numerosArray[1][1] + "\n \t \t" + numerosArray[2][2]);
+		System.out.print(numerosArray[0][0] + "  " + numerosArray[1][1] + "  " + numerosArray[2][2]);
  
 	}
 
