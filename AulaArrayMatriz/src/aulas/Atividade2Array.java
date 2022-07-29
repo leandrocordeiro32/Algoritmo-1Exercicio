46a2465c0fcc5ad5e96eafcfd8b1895403ccb4e0
leandrocordeiro32 <leandro.cordeiro32@gmail.com>
2022-07-29 02:36:55 -0300

    Atividade Array

--- AulaArrayMatriz/src/aulas/Atividade2Array.java
+++ AulaArrayMatriz/src/aulas/Atividade2Array.java
@@ -15,20 +15,34 @@ public class Atividade2Array {
 	String[] nome = new String [5];
 	
 	for (int i = 0 ; i < 5 ; i++) {
+		
 		System.out.print("Informe seu nome: ");
 		nome[i] = sc.nextLine();
+		while (nome[i].length() < 3) {
+			System.out.print("Nome inválido! Digite o nome correto: ");
+			nome[i] = sc.nextLine();
+		}
 		System.out.print("Informe sua idade: ");
 		idade[i] = sc.nextInt();
+		while (idade[i] < 0 || idade[i] >= 150) {
+			System.out.print("Idade inválida! Digite a idade correta: ");
+			idade[i] = sc.nextInt();
+		}
+		
 		System.out.print("Informe sua altura: ");
 		altura[i] = sc.nextDouble();
+		while (altura[i] <= 0 || altura[i] >= 3) {
+			System.out.println("Altura inválida! Informe a altura correta: ");
+			altura[i] = sc.nextDouble();
+		}
 		sc.nextLine();
-		System.out.println("");
+		System.out.println("==========================================");
 		
 	}
 	
 	for (int i = 0 ; i < 5 ; i++) {
 		System.out.println("--------------------------------------------");
-		System.out.println(nome[i] + " tem " + idade[i] + " anos e mede " + altura[i] + " metros");
+		System.out.println(nome[i] + " tem " + idade[i] + " anos e mede " + altura[i] + " metros.");
 		System.out.println("--------------------------------------------");
 	}
 
